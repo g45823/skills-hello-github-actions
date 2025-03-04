a4dde8a79c4f35e6953648227190fe8ec1270aa4
g45823 <131951390+g45823@users.noreply.github.com>
2025-03-05 00:18:03 +0900

    Update read_write.R

--- read_write.R
+++ read_write.R
@@ -2,9 +2,5 @@
 
 a=read.table("./a.txt",skip=2)
 
-b=a
-b[,2]=b[,2]*2
-
-
-write.table(x=b,file="./b2.txt",row.names=F,col.names=F,quote=F)
+write.table(x=a,file="./b2.txt",row.names=F,col.names=F,quote=F)
 
